8366b763bb48248e4dd550fbccac53c7fa10ca02
huy.le3 <huy.le3@mservice.com.vn>
2024-01-23 11:22:47 +0700

    use j2v8

--- app/src/main/java/com/example/androidnode/MainActivity.kt
+++ app/src/main/java/com/example/androidnode/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.androidnode
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.eclipsesource.v8.V8
 import com.example.androidnode.databinding.ActivityMainBinding
 
 
@@ -10,12 +14,26 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.res = "22"
+
+        val runtime = V8.createV8Runtime()
+        val result = runtime.executeIntegerScript(
+            """
+          var first = 5;
+          var second = 1;
+          first << second;
+          
+          """.trimIndent()
+        )
+        binding.res = "5 << 1 = $result"
+        runtime.release()
+
     }
 
     /**
